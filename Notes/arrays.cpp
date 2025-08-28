ec4918f581d952c7b78084d93f41d5f73ca4e23e
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-08-28 11:50:01 -0600

    arrays

--- Notes/arrays.cpp
+++ Notes/arrays.cpp
@@ -14,22 +14,32 @@
 //      cout << fam[i] << " Larsen\n";
 //      if (fam[i] == sibs[i-2])
 //          cout << "They are a sibling!\n";
+//  }
 
 
 //         How do you use an array as an argument in a function
-//
+//  function(string list[])
 
 
 //                           What is type_t
-//
+//  A data type used to represent the size of something
+//  A placeholder used to know how much space we have or when we dont know how much space we have
 
 
 //                       Why would we use type_t
-//
+//  To know how much space we have or when we dont know how much space we have
 
 
 //                     How do you search an array
-//
+//int search(string list[], int len, string item){
+//  for(int i=0; i < len; i++){
+//      if (list[i] == item){
+//          cout << "You are a sibling!\n";
+//          return 1;
+//      }
+//  }
+//  cout << "You are a parent!\n";
+//  return 0;
 
 
 //                      How do you sort an array
@@ -40,20 +50,44 @@
 
 
 #include <iostream>
+#include <limits>
 using namespace std;
 
 string fam[6] = {"Timothy","Sandra","Zachary","Nicholas","Samuel","Bethany"};
 string sibs[4] = {"Zachary","Nicholas","Samuel","Bethany"};
 
-void search(string list, string item){
-    cout << list << endl;
+int search(string list[], size_t len, string item){
+    for(int i=0; i < len; i++){
+        if (list[i] == item){
+            cout << "You are a sibling!\n";
+            return 1;
+        }
+    }
+    cout << "You are a parent!\n";
+    return 0;
+    
 };
 
 int main(){
     for (int i=0; i < size(fam);i++) {
         cout << fam[i] << " Larsen\n";
-        if (fam[i] == sibs[i-2])
-            cout << "They are a sibling!\n";
-        
+        search(sibs, size(sibs), fam[i]);  
     }
+
+
+    //  SIZE OF T
+    cout << numeric_limits <long long>::min() << endl;
+    cout << numeric_limits <long long>::max() << endl;
+    cout << numeric_limits <size_t>::min() << endl;
+    cout << numeric_limits <size_t>::max() << endl;
+
+
+    //Structured Binding
+    auto [d,w,h,v,d,l] = fam;
+    cout << d << endl;
+
+    //Multidimensional arrays
+    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+    return 0;
 }
