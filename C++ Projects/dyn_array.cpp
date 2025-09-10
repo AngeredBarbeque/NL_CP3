ae6289658164979f7e3d1723f62d70886b0b0b1a
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-09-10 11:17:56 -0600

    split into multiple functions

--- C++ Projects/dyn_array.cpp
+++ C++ Projects/dyn_array.cpp
@@ -1,9 +1,42 @@
 // NL Dynamic Arrays
 
+//Includes needed things
 #include <iostream>
 #include <memory>
 using namespace std;
 
+
+//Creates a variable that tracks the current capacity of the array
+int capacity = 1;
+//Makes a unique pointer called colors pointing to a new string
+shared_ptr <string[]> colors(new string[capacity]);
+//Tracks the current amounts of entries
+int entries = 0;
+
+//Function that asks user for colors, and prints out list
+void ask() {
+    while (true) {
+        //Gets a user input
+        cout << "Color:";
+        getline(cin, colors[entries]);
+        if (colors[entries] == "0")
+            break;
+        entries++;
+        //Expands capacity if necessary
+        if (entries == capacity) {
+            capacity++;
+            shared_ptr <string[]> temp(new string[capacity]);
+            for (int i =0; i < entries; i++)
+            temp[i] = colors[i];
+            colors = temp;
+        }
+    }
+    for (int i = 0; i < entries; i++)
+        cout << colors[i] << endl;
+}
+
+
+// Main function
 int main() {
     while (true) {
     cout << "You will be asked to enter as many colors as possible. When you cannot list any more colors, please enter \"0\"" << endl;
@@ -15,28 +48,7 @@ int main() {
         cout << "Goodbye!";
         exit(0);
     }
-    //Creates a variable that tracks the current capacity of the array
-    int capacity = 1;
-    //Makes a unique pointer called colors pointing to a new string
-    shared_ptr <string[]> colors(new string[capacity]);
-    //Tracks the current amounts of entries
-    int entries = 0;
-
-    while (true) {
-        cout << "Color:";
-        getline(cin, colors[entries]);
-        if (colors[entries] == "0")
-            break;
-        entries++;
-        if (entries == capacity) {
-            capacity++;
-            shared_ptr <string[]> temp(new string[capacity]);
-            for (int i =0; i < entries; i++)
-            temp[i] = colors[i];
-            colors = temp;
-        }
-    }
-    for (int i = 0; i < entries; i++)
-        cout << colors[i] << endl;
-    }
+    ask();
+    return 0;
+};
 };
