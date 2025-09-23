7a7ec0fc6f1b93fb39ab9e604ac0127b3095734b
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-09-23 11:49:02 -0600

    4D dracula

--- C++ Projects/pokemon.cpp
+++ C++ Projects/pokemon.cpp
@@ -2,14 +2,22 @@
 
 #include <vector>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+enum First {
+    Fight,
+    Search,
+    Heal,
+    Exit
+};
 
 //An array of arrays, each smaller array has 2 parts
 //Attacking type, Defending Type
 
-REPLACE GRASS WITH ANCIENT
-string type_strong[4][2] = {{"Fire","Grass"},{"Water","Fire"},{"Grass","Water"},{"Electric","Water"}};
-string type_weak[8][2] = {{"Fire","Fire"},{"Fire","Water"},{"Water","Water"},{"Water","Grass"},{"Grass","Fire"},{"Grass","Grass"},{"Electric","Grass"},{"Electric","Electric"}};
+string type_strong[7][2] = {{"Water","Fire"},{"Fire","Ancient"},{"Ancient","Ancient"},{"Dracula","Normal"},{"Dracula","Water"},{"Dracula","Fire"},{"Dracula","Ancient"}};
+string type_weak[8][2] = {{"Fire","Fire"},{"Fire","Water"},{"Water","Water"},{"Water","Ancient"},{"Normal","Dracula"},{"Water","Dracula"},{"Fire","Dracula"},{"Ancient","Dracula"}};
 
 //A struct defining what an attack is
 struct Attack {
@@ -32,7 +40,7 @@ vector<Pokemon> make_pokemon() {
     smith_attacks.push_back({"Pay Cut", 1, "Normal"});
     smith_attacks.push_back({"Cart Bash", 5, "Normal"});
     smith_attacks.push_back({"Flamethrower", 25, "Fire"});
-    Pokemon smithijon = {"Smithijon", 250, 250, smith_attacks, 1, "Normal"};
+    Pokemon smithijon = {"Smithijon", 350, 350, smith_attacks, 1, "Normal"};
 
     vector<Attack> pyro_attacks;
     pyro_attacks.push_back({"Flamethrower", 25, "Fire"});
@@ -44,20 +52,69 @@ vector<Pokemon> make_pokemon() {
     cod_attacks.push_back({"Flop", 0, "Water"});
     cod_attacks.push_back({"Medieval Saltcod Industry", 50, "Water"});
     cod_attacks.push_back({"Slap", 20, "Normal"});
-    Pokemon cod = {"Smithijon", 150, 150, cod_attacks, 1, "Water"};
+    Pokemon cod = {"Cod", 200, 200, cod_attacks, 1, "Water"};
 
     vector<Attack> otto_attacks;
     otto_attacks.push_back({"Armed forces", 75, "Ancient"});
     otto_attacks.push_back({"Slap", 20, "Normal"});
     otto_attacks.push_back({"90 Year old Beef", 40, "Ancient"});
-    Pokemon otto_von_bismark = {"Otto Von Bismark", 200, 200, smith_attacks, 1, "Ancient"};
+    Pokemon otto_von_bismark = {"Otto Von Bismark", 200, 200, otto_attacks, 1, "Ancient"};
 
-    FINISH THIS ONE
-    vector<Attack> smith_attacks;
-    smith_attacks.push_back({"Pay Cut", 1, "Normal"});
-    smith_attacks.push_back({"Cart Bash", 5, "Normal"});
-    smith_attacks.push_back({"Flamethrower", 25, "Fire"});
-    Pokemon smithijon = {"Smithijon", 200, 200, smith_attacks, 1, "Electric"};
+    vector<Attack> dracula_attacks;
+    dracula_attacks.push_back({"Chomp", 150, "Dracula"});
+    dracula_attacks.push_back({"Dracula Beam", 200, "Dracula"});
+    dracula_attacks.push_back({"True Form", 10000, "Dracula"});
+    Pokemon dracula = {"4D Dracula", 500, 500, dracula_attacks, 1, "Dracula"};
+    
+    vector<Pokemon> species;
+    species.push_back(smithijon);
+    species.push_back(pyro);
+    species.push_back(cod);
+    species.push_back(otto_von_bismark);
+    species.push_back(dracula);
+
+    return species;
+}
+
+int random(int range) {
+    int seconds = time(nullptr);
+    srand(seconds);
+    int my_num = rand() % range;
+    return my_num + 1;
+}
 
 
+int main() {
+    cout << "Hello! welcome to POKE MEN.";
+    cout << "You need to select a starter pokemon!";
+    cout << "1: Smithijon, the Normal type Clerk POKEMAN.\n A POKEMAN that works at a grocery store.\n He has exceptional health, but subpar damage capabilities.\n";
+    cout << "2: Pyro, the Fire type TF2 pokemon.\n A POKEMAN with a flamethrower who excels at dealing damage.\n";
+    cout << "3: Cod, the Water type Fish POKEMAN.\n A POKEMAN who has been vital to the history of the world.\n Capable of utilizing the entire Medieval Saltcod Industry.\n";
+    cout << "4: Otto Von Bismark, the Ancient type Historical Figure POKEMAN.\n A POKEMAN who once unified Germany."
+
+
+
+
+    int choice;
+    while (true) {
+        cout << "What would you like to do?";
+        cout << "1: Fight";
+        cout << "2: Search";
+        cout << "3: Heal";
+        cout << "4: Exit";
+        cin >> choice;
+        if (choice == First::Fight) {
+            
+
+        } else if (choice == First::Fight) {
+
+
+        } else if (choice == First::Fight) {
+
+
+        } else {
+
+        };
+    }
+    return 0;
 }
