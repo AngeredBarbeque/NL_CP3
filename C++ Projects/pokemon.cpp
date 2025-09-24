1da50e8352d043ce17693c0d69ccd715fd4526f2
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-09-24 11:48:07 -0600

    catching and main done

--- C++ Projects/pokemon.cpp
+++ C++ Projects/pokemon.cpp
@@ -6,7 +6,15 @@
 #include <ctime>
 using namespace std;
 
-enum First {
+enum Start {
+    Smithijon,
+    Pyro,
+    Cod,
+    Otto,
+    Dracula
+};
+
+enum Main {
     Fight,
     Search,
     Heal,
@@ -16,6 +24,8 @@ enum First {
 //An array of arrays, each smaller array has 2 parts
 //Attacking type, Defending Type
 
+
+string names[10] = {"Otto Von Bismark","George Washington","Andrew Jackson","Chester A. Arthur","President Spider-man","Ms. Larose","Voncent","Dodo Bird","J O N A S","Archduke Franz Ferdinand"};
 string type_strong[7][2] = {{"Water","Fire"},{"Fire","Ancient"},{"Ancient","Ancient"},{"Dracula","Normal"},{"Dracula","Water"},{"Dracula","Fire"},{"Dracula","Ancient"}};
 string type_weak[8][2] = {{"Fire","Fire"},{"Fire","Water"},{"Water","Water"},{"Water","Ancient"},{"Normal","Dracula"},{"Water","Dracula"},{"Fire","Dracula"},{"Ancient","Dracula"}};
 
@@ -80,21 +90,71 @@ int random(int range) {
     int seconds = time(nullptr);
     srand(seconds);
     int my_num = rand() % range;
-    return my_num + 1;
+    return my_num;
+}
+
+void catching(vector<Pokemon> owned, vector<Pokemon> species) {
+    Pokemon opponent = species[random(5)];
+    int choice;
+    cout << "You encountered a " << opponent.name << "!\n What would you like to do?";
+    cout << "1: Catch it\n";
+    cout << "2: Run away";
+    cin >> choice;
+    if (choice == '1') {   
+        cout << "You threw a POKEDODECAHEDRON!\n";
+        if (random(5) == 0) {
+            cout << "You caught it!\n";
+            owned.push_back(opponent);
+            return;
+        }
+        else {
+            cout << opponent.name << " ran away! :(\n";
+            return;
+        }
+    }else {
+        cout << "You ran away!\n";
+        return;
+    }
+}
+
+void battle(vector<Pokemon> owned, vector<Pokemon> species) {
+    Pokemon opponent = species[random(5)];
+    string trainer = names[random(10)];
+    cout << "You encountered " << trainer << "!\n << They send out a " << opponent.name << "!\n";
+    int choice;
+    CREATE IF HERE
 }
 
 
 int main() {
-    cout << "Hello! welcome to POKE MEN.";
-    cout << "You need to select a starter pokemon!";
-    cout << "1: Smithijon, the Normal type Clerk POKEMAN.\n A POKEMAN that works at a grocery store.\n He has exceptional health, but subpar damage capabilities.\n";
-    cout << "2: Pyro, the Fire type TF2 pokemon.\n A POKEMAN with a flamethrower who excels at dealing damage.\n";
-    cout << "3: Cod, the Water type Fish POKEMAN.\n A POKEMAN who has been vital to the history of the world.\n Capable of utilizing the entire Medieval Saltcod Industry.\n";
-    cout << "4: Otto Von Bismark, the Ancient type Historical Figure POKEMAN.\n A POKEMAN who once unified Germany."
-
-
-
-
+    vector<Pokemon> species = make_pokemon(); 
+    vector<Pokemon> owned;
+    int start_choice;
+    while (true) {
+        cout << "Hello! welcome to POKE MEN.\n";
+        cout << "You need to select a starter pokemon!\n";
+        cout << "1: Smithijon, the Normal type Clerk POKEMAN.\n A POKEMAN that works at a grocery store.\n He has exceptional health, but subpar damage capabilities.\n";
+        cout << "2: Pyro, the Fire type TF2 POKEMAN.\n A POKEMAN with a flamethrower who excels at dealing damage.\n";
+        cout << "3: Cod, the Water type Fish POKEMAN.\n A POKEMAN who has been vital to the history of the world.\n Capable of utilizing the entire Medieval Saltcod Industry.\n";
+        cout << "4: Otto Von Bismark, the Ancient type Historical Figure POKEMAN.\n A POKEMAN who once unified Germany.\n Capable of utilizing beef.\n";
+        cout << "5: 4D Dracula.\n Dracula.\n";
+        cin >> start_choice;
+        if (start_choice == Start::Smithijon) {
+            owned.push_back(species[0]);
+        }else if (start_choice == Start::Pyro) {
+            owned.push_back(species[1]);
+        }else if (start_choice == Start::Cod) {
+            owned.push_back(species[2]);
+        }else if (start_choice == Start::Otto) {
+            owned.push_back(species[3]);
+        }else if (start_choice == Start::Dracula) {
+            owned.push_back(species[4]);
+        }else {
+            cout << "Please enter a valid number.\n";
+            continue;
+        }
+    }
+    cout << "Yay! " << owned[0].name << " has joined your party!\n";
     int choice;
     while (true) {
         cout << "What would you like to do?";
@@ -103,18 +163,24 @@ int main() {
         cout << "3: Heal";
         cout << "4: Exit";
         cin >> choice;
-        if (choice == First::Fight) {
+        if (choice == Main::Fight) {
             
-
-        } else if (choice == First::Fight) {
-
-
-        } else if (choice == First::Fight) {
+        } else if (choice == Main::Search) {
 
 
-        } else {
-
-        };
+        } else if (choice == Main::Heal) {
+            for (Pokemon i : owned) {
+                i.cur_hp = i.max_hp;
+                cout << i.name << " has been healed to " << i.cur_hp << "!\n";
+            }
+            continue;
+        } else if (choice == Main::Exit) { 
+            cout << "Bye!\n";
+            return 0;
+        }else {
+            cout << "Please enter a valid number input.\n";
+            continue;
+        }
     }
     return 0;
 }
