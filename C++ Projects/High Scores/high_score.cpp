ed75afe3535078b5a04df61286191f8a6e0fad66
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-10-01 11:49:33 -0600

    :(

--- C++ Projects/High Scores/high_score.cpp
+++ C++ Projects/High Scores/high_score.cpp
@@ -2,6 +2,10 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <limits>
 
 using namespace std;
 
@@ -16,3 +20,43 @@ struct Highscore {
     int score;
     Date date;
 };
+
+void writeScore(Highscore score) {
+    ofstream ofile;
+    ofile.open("data.csv");
+    if(ofile.is_open()){
+        ofile << "name,score,date\n" << score.username << ',' << score.score << ',' << score.date.day << '/' << score.date.month << '/' << score.date.year;
+        ofile.close();
+    }
+};
+
+vector<Highscore> readScore() {
+    ifstream ifile;
+    ifile.open("scores.csv");
+    vector<Highscore> scores;
+    string line;
+    if (ifile.is_open()) {
+        getline(ifile, line);
+        while(getline(ifile, line)){
+            string str;
+            istringstream iss(line);
+            Highscore score;
+            getline(iss, str, ',');
+            score.username = str;
+
+            getline(iss, str, ',');
+            score.score = stoi(str);
+
+            getline(iss, str, ',');
+            //CREATE THIS
+
+            scores.push_back(score);
+        }
+    ifile.close();
+    for (Highscore i : score) {
+        cout << i.username << " : " << i.score << " : " << //Make dates printable
+    }
+   }
+
+   return 0;
+}
