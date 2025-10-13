0fe7f5deca1610f1a811a7a615d4698ce5ffd83b
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-10-13 11:48:10 -0600

    Notes start

---
+++ Notes/Python/Parent and Child Objects/main.py
@@ -0,0 +1,29 @@
+# What is a parent/abstract class?
+# A class with attributes that all child classes also have
+
+# How do you create a child class?
+#
+
+# How does a child class inherit from the parent class?
+# 
+
+# What are class diagrams?
+# Diagrams that assist in visualizing things about a class, its attributes, and its methods.
+# They are mainly useful in seeing how classes interact with each other.
+
+# How are class diagrams used to show a parent/child relationship?
+# They can use arrows to connect them to show inheritence
+
+# How do you overload operators in a class?
+#
+
+# What are test cases?
+#
+
+# Why do we use test cases?
+#
+
+# How do we create test cases?
+#
+
+
