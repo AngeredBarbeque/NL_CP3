95f139ff3a47fa9d92e3a832be269956185d98ae
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-10-27 19:54:10 -0600

    chess done

--- Python Projects/Chess/main.py
+++ Python Projects/Chess/main.py
@@ -1,6 +1,6 @@
 from pieces import *
-from InquirerPy import inquirer
 
+#Creates all pieces and returns a chessGame with all pieces.
 def pieces():
     wpawna = Pawn("white", Position(1,2,"♙"))
     wpawnb = Pawn("white", Position(2,2,"♙"))
@@ -36,7 +36,7 @@ def pieces():
     bking = King("black",Position(5,8,"♚"))
     bqueen = Queen("black",Position(4,8,"♛"))
 
-    piece_list = [
+    black_list = [
     bpawna,
     bpawnb,
     bpawnc,
@@ -52,28 +52,31 @@ def pieces():
     bbishopa,
     bbishopb,
     bking,
-    bqueen,
-    wpawna,
-    wpawnb,
-    wpawnc,
-    wpawnd,
-    wpawne,
-    wpawnf,
-    wpawng,
-    wpawnh,
-    wrooka,
-    wrookb,
-    wknighta,
-    wnkightb,
-    wbishopa,
-    wbishopb,
-    wking,
-    wqueen
+    bqueen
     ]
-    return piece_list
+    white_list = [
+        wpawna,
+        wpawnb,
+        wpawnc,
+        wpawnd,
+        wpawne,
+        wpawnf,
+        wpawng,
+        wpawnh,
+        wrooka,
+        wrookb,
+        wknighta,
+        wnkightb,
+        wbishopa,
+        wbishopb,
+        wking,
+        wqueen
+    ]
+    game = ChessGame(white_list, black_list)
+    return game
 
 
-
+#Prints the board in an appealing way
 def printBoard(piece_list):
     print(' -------------------------')
     for row in range(8,0,-1):
@@ -88,16 +91,19 @@ def printBoard(piece_list):
             else:
                 print(f" |{symbol}",end='')
 
-
+#Moves pieces
 def main():
-    piece_list = pieces()
-    choices = []
+    game = pieces()
+    piece_list = game.getPieces()
     printBoard(piece_list)
-    print("--------- White's Turn! ---------")
-    for i in piece_list:
-        if i.color == "white":
-            choices.append(i)
-        
+    game.move(game.black_pieces[0], Position(1,6))
+    game.move(game.black_pieces[4],Position(5,6))
+    game.move(game.white_pieces[10],Position(3,3))
+    game.move(game.black_pieces[15],Position(4,1))
+    game.move(game.white_pieces[12],Position(7,5))
+    print("Moved Pieces!")
+    piece_list = game.getPieces()
+    printBoard(piece_list)
 
     
 
--- Python Projects/Chess/pieces.py
+++ Python Projects/Chess/pieces.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+#Position object with column, row, and symbol
 class Position():
     def __init__(self, col, row, sym=" "):
         self.col = col
@@ -21,11 +22,35 @@ class Position():
         else:
             return False
 
+#Chess game consisting of two lists of white and black pieces
 class ChessGame():
     def __init__(self, white_pieces, black_pieces):
         self.white_pieces = white_pieces
         self.black_pieces = black_pieces
+    def move(self, piece, pos):
+            before_piece = self.getPieceAt(pos)
+            if before_piece:
+                print(f"{before_piece.symbol()}  caputured by {piece.symbol()} !")
+                self.remove(before_piece)
+            piece.pos.row = pos.row
+            piece.pos.col = pos.col
+    def remove(self, piece):
+        if piece.color == "black":
+            self.black_pieces.remove(piece)
+        else:
+            self.white_pieces.remove(piece)
+    def getPieces(self):
+        return self.black_pieces + self.white_pieces
+    def getPieceAt(self, pos):
+        for i in self.white_pieces:
+            if i.pos.row == pos.row and i.pos.col == pos.col:
+                return i
+        for i in self.black_pieces:
+            if i.pos.row == pos.row and i.pos.col == pos.col:
+                return i
+        return 0
 
+#abstract class for chess pieces
 class ChessPiece(ABC):
     def __init__(self, color, pos):
         self.color = color
@@ -33,12 +58,13 @@ class ChessPiece(ABC):
     def getPos(self):
         return self.pos
     @abstractmethod
-    def move(self,new_pos):
+    def canMoveTo(self,new_pos):
         pass
     @abstractmethod
     def symbol(self):
         pass
 
+#Pawn class
 class Pawn(ChessPiece):
     def __init__(self, color, pos):
         super().__init__(color, pos)
@@ -56,11 +82,9 @@ class Pawn(ChessPiece):
         else:
             return False
     def symbol(self):
-        return "p"
-    def move(self,new_pos):
-        new_pos.symbol = self.symbol()
-        self.pos = new_pos
-    
+        return self.pos.sym
+
+#rook class
 class Rook(ChessPiece):
     def __init__(self, color, pos):
         super().__init__(color, pos)
@@ -70,11 +94,9 @@ class Rook(ChessPiece):
         else:
             return False
     def symbol(self):
-        return "r"
-    def move(self,new_pos):
-        new_pos.symbol = self.symbol()
-        self.pos = new_pos
+        return self.pos.sym
 
+#knight class
 class Knight(ChessPiece):
     def __init__(self, color, pos):
         super().__init__(color, pos)
@@ -84,11 +106,9 @@ class Knight(ChessPiece):
         elif abs(self.pos.col - new_pos.col) == 2 and abs(self.pos.row - new_pos.row) == 1 and new_pos.row <=8 and new_pos.col <=8:
             return False
     def symbol(self):
-        return "k"
-    def move(self,new_pos):
-        new_pos.symbol = self.symbol()
-        self.pos = new_pos
+        return self.pos.sym
 
+#bishop class
 class Bishop(ChessPiece):
     def __init__(self, color, pos):
         super().__init__(color, pos)
@@ -98,11 +118,9 @@ class Bishop(ChessPiece):
         else:
             return False
     def symbol(self):
-        return "b"
-    def move(self,new_pos):
-        new_pos.symbol = self.symbol()
-        self.pos = new_pos
+        return self.pos.sym
 
+#queen class
 class Queen(ChessPiece):
     def __init__(self, color, pos):
         super().__init__(color, pos)
@@ -112,11 +130,9 @@ class Queen(ChessPiece):
         else:
             return False
     def symbol(self):
-        return "Q"
-    def move(self,new_pos):
-        new_pos.symbol = self.symbol()
-        self.pos = new_pos
-    
+        return self.pos.sym
+
+#king class
 class King(ChessPiece):
     def __init__(self, color, pos):
         super().__init__(color, pos)
@@ -126,7 +142,4 @@ class King(ChessPiece):
         else:
             return False
     def symbol(self):
-        return "K"
-    def move(self,new_pos):
-        new_pos.symbol = self.symbol()
-        self.pos = new_pos
+        return self.pos.sym
