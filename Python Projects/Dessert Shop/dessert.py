7cbb063caa6648c903ebfadea09c508d343455f4
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-10-28 11:48:14 -0600

    Part 1!

--- Python Projects/Dessert Shop/dessert.py
+++ Python Projects/Dessert Shop/dessert.py
@@ -1,35 +1,53 @@
 #Creates a parent classes for all other desserts
-class DessertItem():
-    def __init__(self,name=""):
+from abc import ABC, abstractmethod
+class DessertItem(ABC):
+    def __init__(self,name="",tax_percent=7.25):
         self.name = name
-
+        self.tax_percent = tax_percent
+    @abstractmethod
+    def calculateCost(self):
+        pass
+    def calculateTax(self):
+        return self.calculateCost() * (self.tax_percent / 100)
 #Creates a candy class
 class Candy(DessertItem):
-    def __init__(self, name="", price_per_pound=0.0,amount=0.0):
-        super().__init__(name)
+    def __init__(self, name="", tax_percent=7.25, price_per_pound=0.0,amount=0.0):
+        super().__init__(name,tax_percent)
         self.price_per_pound = price_per_pound
         self.amount = amount
+    def calculateCost(self):
+        return self.amount * self.price_per_pound
 
 #Creates an ice cream class
 class IceCream(DessertItem):
-    def __init__(self, name="",price_per_scoop=0.0,scoops=0):
-        super().__init__(name)
+    def __init__(self, name="", tax_percent=7.25,price_per_scoop=0.0,scoops=0):
+        super().__init__(name,tax_percent)
         self.price_per_scoop = price_per_scoop
         self.scoops = scoops
+    def calculateCost(self):
+        return self.scoops * self.price_per_scoop
 
 #Creates a cookie class
 class Cookies(DessertItem):
-    def __init__(self, name="", price_per_dozen=0.0, amount=0):
-        super().__init__(name)
+    def __init__(self, name="", tax_percent=7.25, price_per_dozen=0.0, amount=0):
+        super().__init__(name,tax_percent)
         self.price_per_dozen = price_per_dozen
         self.amount = amount
+    def calculateCost(self):
+        dozens = self.amount/12
+        return dozens * self.price_per_dozen
 
 #Creates a sundae class, ice cream child class
 class Sundae(IceCream):
-    def __init__(self, name="", price_per_scoop=0.0, scoops=0, topping="",topping_price=0.0):
-        super().__init__(name, price_per_scoop, scoops)
+    def __init__(self, name="", tax_percent=7.25, price_per_scoop=0.0, scoops=0, topping="",topping_price=0.0):
+        super().__init__(name, tax_percent, price_per_scoop, scoops)
         self.topping = topping
         self.topping_price = topping_price
+    def calculateCost(self):
+        scoop_price = self.price_per_scoop * self.scoops
+        price = scoop_price + self.topping_price
+        return price
+
 
 #Creates an order class to hold different desserts
 class Order():
@@ -39,3 +57,13 @@ class Order():
         self.order.append(item)
     def __len__(self):
         return len(self.order)
+    def orderCost(self):
+        subtotal = 0.0
+        for i in self.order:
+            subtotal += i.calculateCost()
+        return subtotal
+    def orderTax(self):
+        taxtotal = 0.0
+        for i in self.order:
+            taxtotal += i.calculateTax()
+        return taxtotal
--- Python Projects/Dessert Shop/dessert_shop.py
+++ Python Projects/Dessert Shop/dessert_shop.py
@@ -1,15 +1,19 @@
 from dessert import *
+from tabulate import tabulate
 
-#Creates an order object containing instances of all child/grandchild classes
+#data = [["Bruce Wayne", "Bat", 2], ["Bruce Payne", "Bat 2", 4], ["Johnny", "Teeth", 34]]
+#print(tabulate(data, headers=["Name", "Misc", "Number"]))
+
+#Creates an order object containing instances of all child/grandchild classes 
 #Prints each item in the order, and then prints the length of it
 def main():
     order = Order()
-    gummy = Candy("Gummy Bears", 1.5, 0.25)
-    corn = Candy("Candy Corn", 0.35, 0.25)
-    chocolate = Cookies("Chocolate Chip", 3.99, 6)
-    oat = Cookies("Oatmeal Raisin", 3.45, 2)
-    pistachio = IceCream("Pistachio",0.79, 2)
-    vanilla = Sundae("Vanilla",0.69,3,"Fudge",1.29)
+    gummy = Candy("Gummy Bears", 10, 1.5, 0.25)
+    corn = Candy("Candy Corn", 10, 0.35, 0.25)
+    chocolate = Cookies("Chocolate Chip", 10, 3.99, 6)
+    oat = Cookies("Oatmeal Raisin", 10, 3.45, 2)
+    pistachio = IceCream("Pistachio", 10, 0.79, 2)
+    vanilla = Sundae("Vanilla", 10, 0.69,3,"Fudge",1.29)
     order.add(gummy)
     order.add(corn)
     order.add(chocolate)
@@ -19,5 +23,14 @@ def main():
     for i in order.order:
         print(i.name)
     print(len(order))
+    data = []
+    for i in order.order:
+        data.append([i.name, f"${round(i.calculateCost(),2)}", f"${round(i.calculateTax(),2)}"])
+    print(tabulate(data, headers=["Item","Cost","Tax"]))
+    print("----------------------------------")
+    print(tabulate([[f"${round(order.orderCost(),2)}", f"${round(order.orderTax(),2)}"]], headers=["Subtotal","Total Tax"]))
+    print("----------------------------------")
+    print(tabulate([[f"${round(order.orderCost(), 2) + round(order.orderTax(), 2)}"]], headers=["Total"]))
+    
 
 main()
