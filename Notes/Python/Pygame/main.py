ace9d357820db9e737df71262cb0a87327637db8
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-10-31 10:56:14 -0600

    notes

---
+++ Notes/Python/Pygame/main.py
@@ -0,0 +1,15 @@
+# Nicholas Larsen
+
+#How do you set up pygame?
+
+#What is the purpose of the "While running" loop?
+
+#How do you create a screen in pygame?
+
+#How are objects placed on the screen in pygame?
+
+#What events can I listen for in pygame? What do those events do?
+
+#How can I detect collision with pygame?
+
+#How do you add sounds in pygame?
